b0d58e1261d2d5cb8c5087fd698b66928635b2a6
JOZEF ANTONY NEELAMKAVIL <67110139+jzf21@users.noreply.github.com>
2023-01-06 22:37:17 +0530

    Revert "more changes 2"

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,5 @@
 import About from "../components/About/Aboutpage";
 import HomePage from "../components/home/Homepage";
-import "tailwindcss/dist/base.css";
 
 export default function Home() {
   return (
