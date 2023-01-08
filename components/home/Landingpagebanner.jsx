e45c2070a18b663785e3bfcd8db54ae52bf3ea51
jam-dev <jamalvga2002@gmail.com>
2023-01-09 00:41:30 +0530

    color changed

--- components/home/Landingpagebanner.jsx
+++ components/home/Landingpagebanner.jsx
@@ -15,7 +15,7 @@ export default function landingpagebanner() {
                     <span className="text-transparent font-display bg-clip-text bg-gradient-to-r from-green-200 to-white"> Take your first step towards getting into your dream institution </span>
                 </h1>
 
-                <a href="#" title="" className="inline-flex items-center px-8 py-4 mt-8 font-semibold text-white transition-all duration-200 bg-[#024751] rounded-lg sm:mt-16 hover:bg-green-800 focus:bg-blue-700" role="button">
+                <a href="#" title="" className="inline-flex items-center px-8 py-4 mt-8 font-semibold text-white transition-all duration-200 bg-[#024751] rounded-lg sm:mt-16 hover:bg-green-800 focus:bg-green-700" role="button">
                     Join Now
                     <svg className="w-6 h-6 ml-4 -mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
