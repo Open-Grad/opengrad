d162a8aebd76034a90d4b990de35c4d507da493b
JOZEF ANTONY NEELAMKAVIL <67110139+jzf21@users.noreply.github.com>
2023-02-06 21:28:37 +0530

    Revert "sliders added"

--- components/home/Homepage.jsx
+++ components/home/Homepage.jsx
@@ -8,24 +8,23 @@ import News from "./News";
 import Offer from "./Offer";
 import Collabe from "./Collabe";
 import Blog from "./Blog";
-import { Responsivehook } from "../Responsivehook";
-import { Carousel1 } from "../Carousels/Carousel1";
-import { CarouselNews } from "../Carousels/CarouselNews";
-import { CarouselOffer } from "../Carousels/CarouselOffer";
+
 const HomePage = () => {
   return (
     <>
       <Navbar />
       <Landingpagebanner />
       <Videosection />
-      <Responsivehook mobile={<Carousel1 />} pc={<Mentors />} />
-      <Responsivehook mobile={<CarouselNews />} pc={<News />} />
+      <Mentors />
+      <News />
       <Offer />
       <Collabe />
-      <Responsivehook mobile={<CarouselOffer />} pc={<Blog />} />
+      <Blog />
       <Footer />
+
+      
     </>
   );
 };
 
-export default HomePage;
+export default HomePage
--- components/home/Mentors.jsx
+++ components/home/Mentors.jsx
@@ -5,7 +5,7 @@ export default function mentors() {
     <section className="py-10 bg-white sm:py-16 lg:py-24">
     <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="max-w-2xl mx-auto text-center">
-            <h2 className="text-3xl font-bold leading-tight font-display text-gray-800 sm:text-4xl lg:text-5xl">Testimonials <span className="text-blue-600"></span> </h2>
+            <h2 className="text-3xl font-bold leading-tight font-display text-gray-800 sm:text-4xl lg:text-5xl">Mentor Speaks  <span className="text-blue-600"></span> </h2>
         </div>
 
         <div className="grid max-w-xl grid-cols-1 mx-auto mt-8 text-center lg:max-w-full sm:mt-12 lg:mt-20 lg:grid-cols-3 gap-x-6 xl:gap-x-12 gap-y-6">
@@ -70,9 +70,6 @@ export default function mentors() {
             </div>
         </div>
     </div>
-
-
-    
 </section>
 
 
