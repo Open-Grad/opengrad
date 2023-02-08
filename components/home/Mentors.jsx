b254f393a4a220fd4c77c1b35536d048415912fb
zodwick <115868723+zodwick@users.noreply.github.com>
2023-02-09 00:34:33 +0530

    responsive

--- components/home/Mentors.jsx
+++ components/home/Mentors.jsx
@@ -4,7 +4,7 @@ export default function mentors() {
  
 
   return (
-    <section className=" px-8 pb-16 pt-16 bg-gray-50 ">
+    <section className=" m:px-16 px-0 pb-16 pt-16 bg-gray-50 ">
       <div className="flex flex-col items-center p-0">
         <p className="text-lg font-medium text-gray-600 font-pj">
           World class Service
@@ -18,7 +18,7 @@ export default function mentors() {
 
       <div class="scrollbar-hide mt-14 flex w-full snap-x snap-mandatory scroll-px-10 gap-10 overflow-x-scroll scroll-smooth px-10  ">
         <div class="md:2/3 relative aspect-[3/3] w-[90%] shrink-0 snap-start snap-always rounded-xl sm:w-[44%] md:w-[30%]">
-          <div className="flex flex-col rounded-2xl overflow-hidden shadow-xl shadow-[#d8fee243]">
+          <div className="flex flex-col rounded-2xl overflow-hidden shadow-xl shadow-[#d8fee243] h-full">
             <div className="flex flex-col justify-between flex-1 p-6 bg-white lg:py-8 lg:px-7">
               <div className="flex-1">
                 <blockquote className="flex-1 mt-8">
@@ -38,8 +38,8 @@ export default function mentors() {
               <div className="flex items-center mt-8">
                 <img
                   className="flex-shrink-0 object-cover rounded-full w-11 h-11"
-                  src="https://cdn.rareblocks.xyz/collection/clarity/images/testimonial/4/avatar-male-2.png"
-                  alt=""
+                  src="/images/avatars/reshi.jpg"
+                  alt="Reshi"
                 />
                 <div className="ml-4">
                   <p className="text-base font-bold text-gray-900 font-pj">
@@ -55,13 +55,15 @@ export default function mentors() {
         </div>
 
         <div class="md:2/3 relative aspect-[3/3] w-[90%] shrink-0 snap-start snap-always rounded-xl sm:w-[44%] md:w-[30%]">
-          <div className="flex flex-col rounded-2xl overflow-hidden shadow-xl shadow-[#d8fee243]">
-            <div className="flex flex-col justify-between flex-1 p-6 bg-white lg:py-8 lg:px-7">
+          <div className="flex flex-col rounded-2xl overflow-hidden shadow-xl shadow-[#d8fee243]  h-full">
+            <div className="flex flex-col justify-center flex-1 p-6 bg-white lg:py-8 lg:px-7">
               <div className="flex-1">
                 <blockquote className="flex-1 mt-8">
-                  <p className="text-lg leading-relaxed text-gray-900 font-pj">
-                    “Simply the best. Better than all the rest. I’d recommend
-                    this product to beginners and advanced users.”
+                  <p className="text-2sm leading-relaxed text-gray-900 font-pj">
+                  &quot; When money is removed from ed-tech, competitive exams become a lot easier, because they are way more easier 
+                  than what the aspirants think. When you have mentors who have gone through what you are going through right
+                  now the journey will be enjoyable and fun. I feel that OpenGrad will revolutionize the edtech space and provide
+                  a level ground for all the aspirants.&quot;
                   </p>
                 </blockquote>
               </div>
@@ -69,15 +71,15 @@ export default function mentors() {
               <div className="flex items-center mt-8">
                 <img
                   className="flex-shrink-0 object-cover rounded-full w-11 h-11"
-                  src="https://cdn.rareblocks.xyz/collection/clarity/images/testimonial/4/avatar-male-2.png"
+                  src="/images/avatars/rohit.jpg"
                   alt=""
                 />
                 <div className="ml-4">
                   <p className="text-base font-bold text-gray-900 font-pj">
-                    Jacob Jones
+                    Rohit Pradeep
                   </p>
                   <p className="mt-0.5 text-sm font-pj text-gray-600">
-                    Digital Marketer
+                  IIM Bangalore ‘24
                   </p>
                 </div>
               </div>
@@ -85,45 +87,14 @@ export default function mentors() {
           </div>
         </div>
 
+       
         <div class="md:2/3 relative aspect-[3/3] w-[90%] shrink-0 snap-start snap-always rounded-xl sm:w-[44%] md:w-[30%]">
           <div className="flex flex-col rounded-2xl overflow-hidden shadow-xl shadow-[#d8fee243]">
             <div className="flex flex-col justify-between flex-1 p-6 bg-white lg:py-8 lg:px-7">
               <div className="flex-1">
                 <blockquote className="flex-1 mt-8">
                   <p className="text-lg leading-relaxed text-gray-900 font-pj">
-                    “Simply the best. Better than all the rest. I’d recommend
-                    this product to beginners and advanced users.”
-                  </p>
-                </blockquote>
-              </div>
-
-              <div className="flex items-center mt-8">
-                <img
-                  className="flex-shrink-0 object-cover rounded-full w-11 h-11"
-                  src="https://cdn.rareblocks.xyz/collection/clarity/images/testimonial/4/avatar-male-2.png"
-                  alt=""
-                />
-                <div className="ml-4">
-                  <p className="text-base font-bold text-gray-900 font-pj">
-                    Jacob Jones
-                  </p>
-                  <p className="mt-0.5 text-sm font-pj text-gray-600">
-                    Digital Marketer
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="md:2/3 relative aspect-[3/3] w-[90%] shrink-0 snap-start snap-always rounded-xl sm:w-[44%] md:w-[30%]">
-          <div className="flex flex-col rounded-2xl overflow-hidden shadow-xl shadow-[#d8fee243]">
-            <div className="flex flex-col justify-between flex-1 p-6 bg-white lg:py-8 lg:px-7">
-              <div className="flex-1">
-                <blockquote className="flex-1 mt-8">
-                  <p className="text-lg leading-relaxed text-gray-900 font-pj">
-                    “Simply the best. Better than all the rest. I’d recommend
-                    this product to beginners and advanced users.”
+                    “While the majority of the organized coaching institutes were focused on Engineering and Medical entrances,  I found it difficult to connect and  understand the opportunities and preparation strategies when it comes to Law entrance examinations. The concept of Opengrad is promising. This will help a lot of people like me who was struggling for the right direction, mentorship and preparation materials during my entrance days”
                   </p>
                 </blockquote>
               </div>
