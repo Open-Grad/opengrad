5f6bbdddd35a31c50762bb40b7f9f76a30748a09
zodwick <115868723+zodwick@users.noreply.github.com>
2023-02-09 00:06:59 +0530

    css added

--- components/home/Mentors.jsx
+++ components/home/Mentors.jsx
@@ -1,9 +1,8 @@
 import React, { useRef, useState } from "react";
 
-
 export default function mentors() {
+ 
 
-  
   return (
     <section className=" px-8 pb-16 pt-16 bg-gray-50 ">
       <div className="flex flex-col items-center p-0">
@@ -17,16 +16,21 @@ export default function mentors() {
 
       {/* carousel */}
 
-
       <div class="scrollbar-hide mt-14 flex w-full snap-x snap-mandatory scroll-px-10 gap-10 overflow-x-scroll scroll-smooth px-10  ">
         <div class="md:2/3 relative aspect-[3/3] w-[90%] shrink-0 snap-start snap-always rounded-xl sm:w-[44%] md:w-[30%]">
           <div className="flex flex-col rounded-2xl overflow-hidden shadow-xl shadow-[#d8fee243]">
             <div className="flex flex-col justify-between flex-1 p-6 bg-white lg:py-8 lg:px-7">
               <div className="flex-1">
                 <blockquote className="flex-1 mt-8">
-                  <p className="text-lg leading-relaxed text-gray-900 font-pj">
-                    “Simply the best. Better than all the rest. I’d
-                    recommend this product to beginners and advanced users.”
+                  <p className="text-sm leading-relaxed text-gray-900 font-pj">
+                    " I am thrilled to come across this new edtech concept, 
+                    Opengrad bringing together the aspirants, converts and the alumni.
+                     This will give the aspirants  a never before experience. 
+                     This is one of a kind platform that offers top quality content curated by the 
+                     converts and students from premier universities. 
+                     With Opengrad the competitive exams are no longer a monopoly of students with
+                      access to expensive coaching materials, because here the best is offered for free! "
+
                   </p>
                 </blockquote>
               </div>
@@ -39,18 +43,16 @@ export default function mentors() {
                 />
                 <div className="ml-4">
                   <p className="text-base font-bold text-gray-900 font-pj">
-                    Jacob Jones
+                  Reshi Kiran 
                   </p>
                   <p className="mt-0.5 text-sm font-pj text-gray-600">
-                    Digital Marketer
-                  </p>
+                  - IIT Roorkee ‘18               
+                 </p>
                 </div>
               </div>
             </div>
           </div>
         </div>
-
-
 
         <div class="md:2/3 relative aspect-[3/3] w-[90%] shrink-0 snap-start snap-always rounded-xl sm:w-[44%] md:w-[30%]">
           <div className="flex flex-col rounded-2xl overflow-hidden shadow-xl shadow-[#d8fee243]">
@@ -58,8 +60,8 @@ export default function mentors() {
               <div className="flex-1">
                 <blockquote className="flex-1 mt-8">
                   <p className="text-lg leading-relaxed text-gray-900 font-pj">
-                    “Simply the best. Better than all the rest. I’d
-                    recommend this product to beginners and advanced users.”
+                    “Simply the best. Better than all the rest. I’d recommend
+                    this product to beginners and advanced users.”
                   </p>
                 </blockquote>
               </div>
@@ -82,9 +84,6 @@ export default function mentors() {
             </div>
           </div>
         </div>
-
-
-
 
         <div class="md:2/3 relative aspect-[3/3] w-[90%] shrink-0 snap-start snap-always rounded-xl sm:w-[44%] md:w-[30%]">
           <div className="flex flex-col rounded-2xl overflow-hidden shadow-xl shadow-[#d8fee243]">
@@ -92,8 +91,8 @@ export default function mentors() {
               <div className="flex-1">
                 <blockquote className="flex-1 mt-8">
                   <p className="text-lg leading-relaxed text-gray-900 font-pj">
-                    “Simply the best. Better than all the rest. I’d
-                    recommend this product to beginners and advanced users.”
+                    “Simply the best. Better than all the rest. I’d recommend
+                    this product to beginners and advanced users.”
                   </p>
                 </blockquote>
               </div>
@@ -116,9 +115,6 @@ export default function mentors() {
             </div>
           </div>
         </div>
-
-
-
 
         <div class="md:2/3 relative aspect-[3/3] w-[90%] shrink-0 snap-start snap-always rounded-xl sm:w-[44%] md:w-[30%]">
           <div className="flex flex-col rounded-2xl overflow-hidden shadow-xl shadow-[#d8fee243]">
@@ -126,8 +122,8 @@ export default function mentors() {
               <div className="flex-1">
                 <blockquote className="flex-1 mt-8">
                   <p className="text-lg leading-relaxed text-gray-900 font-pj">
-                    “Simply the best. Better than all the rest. I’d
-                    recommend this product to beginners and advanced users.”
+                    “Simply the best. Better than all the rest. I’d recommend
+                    this product to beginners and advanced users.”
                   </p>
                 </blockquote>
               </div>
@@ -150,8 +146,6 @@ export default function mentors() {
             </div>
           </div>
         </div>
-
-        
       </div>
     </section>
   );
